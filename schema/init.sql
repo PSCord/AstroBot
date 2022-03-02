78cb6e4120d52f44bbb8994b4ec35fc769029a93
Dropheart <ahmed@techfromtheheart.com>
2022-03-02 22:40:33 +0300

    fixing syntax

--- schema/init.sql
+++ schema/init.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS events (
   id BIGINT PRIMARY KEY,
   eventroleid BIGINT,
   xp INTEGER
-)
+);
 CREATE TABLE IF NOT EXISTS options (
   beep TEXT DEFAULT 'boop',
   doublexp BOOLEAN DEFAULT FALSE,
